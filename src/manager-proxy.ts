640adfdc452c85ffcdc80d736ad6bb6b47d2c9e5
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2024-11-27 21:53:10 -0500

    revised schema and proxy.ts to capture delegator rewards.  Used ABI from implmentation contract

--- src/manager-proxy.ts
+++ src/manager-proxy.ts
@@ -3,86 +3,79 @@ import {
   SetController as SetControllerEvent,
   Rebond as RebondEvent,
   Bond as BondEvent,
-  EarningsClaimed as EarningsClaimedEvent
-} from "../generated/ManagerProxy/ManagerProxy"
+  EarningsClaimed as EarningsClaimedEvent,
+} from "../generated/ManagerProxy/ManagerProxy";
 import {
   ParameterUpdate,
   SetController,
   Bond,
   EarningsClaimed,
-  Rebond
-} from "../generated/schema"
+  Rebond,
+} from "../generated/schema";
 
 export function handleParameterUpdate(event: ParameterUpdateEvent): void {
   let entity = new ParameterUpdate(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.param = event.params.param
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  );
+  entity.param = event.params.param;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
 }
 
 export function handleSetController(event: SetControllerEvent): void {
   let entity = new SetController(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.controller = event.params.controller
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  );
+  entity.controller = event.params.controller;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
 }
 
 export function handleBond(event: BondEvent): void {
   let entity = new Bond(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.newDelegate = event.params.newDelegate
-  entity.oldDelegate = event.params.oldDelegate
-  entity.delegator = event.params.delegator
-  entity.additionalAmount = event.params.additionalAmount
-  entity.bondedAmount = event.params.bondedAmount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  );
+  entity.newDelegate = event.params.newDelegate;
+  entity.oldDelegate = event.params.oldDelegate ? event.params.oldDelegate : null;
+  entity.delegator = event.params.delegator;
+  entity.additionalAmount = event.params.additionalAmount;
+  entity.bondedAmount = event.params.bondedAmount;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
 }
 
 export function handleEarningsClaimed(event: EarningsClaimedEvent): void {
   let entity = new EarningsClaimed(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.delegate = event.params.delegate
-  entity.delegator = event.params.delegator
-  entity.fees = event.params.fees
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  );
+  entity.delegate = event.params.delegate;
+  entity.delegator = event.params.delegator;
+  entity.rewards = event.params.rewards;
+  entity.fees = event.params.fees;
+  entity.startRound = event.params.startRound;
+  entity.endRound = event.params.endRound;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
 }
 
 export function handleRebond(event: RebondEvent): void {
   let entity = new Rebond(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.delegate = event.params.delegate
-  entity.delegator = event.params.delegator
-  entity.unbondingLockId = event.params.unbondingLockId
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  );
+  entity.delegate = event.params.delegate;
+  entity.delegator = event.params.delegator;
+  entity.unbondingLockId = event.params.unbondingLockId;
+  entity.amount = event.params.amount;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
 }
